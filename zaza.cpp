d455dc3b395646715cb0f37a37ba3566136806e1
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-05-09 12:30:48 +0000

    ex1

---
+++ zaza.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Circulo{
+    private:
+        float raio;
+    public:
+        Circulo(float r){
+            raio=r;
+        }
+        float CalcularArea(){
+            return 3.14 *raio*raio;
+        }
+        float CalcularPerimetro(){
+            return 2*3.14*raio;
+        }
+};
+
+int main(){
+    float raio;
+    std::cout<<"Digite o raio:";
+    std::cin>>raio;
+    Circulo MeuCirculo(raio);
+    std::cout<<"Area do Circulo:"<<MeuCirculo.CalcularArea() <<std::endl;
+    std::cout<<"Perimetro do Circulo:"<<MeuCirculo.CalcularPerimetro()<<std::endl;
+
+
+
+    return 0;
+}

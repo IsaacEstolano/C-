d54eea603150671c8f7c15bae71f93674c2e09a2
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-05-10 21:55:25 +0000

    sla

--- ex4.cpp
+++ ex4.cpp
@@ -8,23 +8,34 @@ class Aluno{
         ~Aluno(){
 
         }
-
-        float CalcularMedia(float ArmazenaNota,float QuantidadeNota){
-            return ArmazenaNota/QuantidadeNota;
+        float CalcularMedia(float ArmazenaNota=0,float QuantidadeNota,float Media){
+            Media=ArmazenaNota/QuantidadeNota;
+            return Media ;
+        }
+        float AdicionarNota(){
+            
         }
         void MostrarDados(){
-            std::cout<<""
+            std::cout<<"Nome:"<<Nome<<std::endl;
+            std::cout<<"Matricula:"<<Matricula<<std::endl;
+            std::cout<<"Nota:"<<Nota<<std::endl;
+            std::cout << "Media: " << CalcularMedia() << std::endl;
+        }
+        void set(float _Matricula,std::string _Nome,float _Nota,float _ArmazenaNota,float _QuantidadeNota){
+            Matricula=_Matricula;
+            Nome=_Nome;
+            Nota=_Nota;
+
         }
     private:
-        float matricula;
-        std::string nome;
-        float nota;
+        float Matricula;
+        std::string Nome;
+        float Nota;
+        float ArmazenaNota;
+        float QuantidadeNota;
 };
-
-
-
-
 int main(){
+    Aluno aluno;
     float Matricula;
     std::string Nome;
     float Nota;
@@ -38,12 +49,16 @@ int main(){
     std::cout<<"Digite quantas notas voce ira armazenar:";
     std::cin>>QuantidadeNota;
     
-     while (QuantidadeNota>0);
+     while (QuantidadeNota>0)
     {
         std::cout<<"Digite sua nota";
         std::cin>>Nota;
-        ArmazenaNota+=Nota;
-        QuantidadeNota--;
+       aluno.AdicionarNota(Nota);
     }
+    
+    aluno.CalcularMedia(ArmazenaNota,QuantidadeNota);
+    aluno.set(Matricula,Nome,Nota);
+    aluno.MostrarDados();
+    
     return 0;
 }

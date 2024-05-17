d4b679c95316352b08dd4a9b8b152be6bba7547e
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-05-17 14:41:38 +0000

    ex6 inicio

--- ex6.cpp
+++ ex6.cpp
@@ -8,25 +8,39 @@ class Funcionario{
         }
         ~Funcionario(){
         }
-        float addAumento(double valor){
+        float addAumento(double valor,float salarioAumento){
             std::cout<<"Digite quanto de aumento ira receber:";
             std::cin>>valor;
-            return salario+=valor;
+            salarioAumento=salario+valor;
+            return salarioAumento;
+        }
+        float ganhoAnual(float ganhoAnual,double valor){
+            ganhoAnual=salarioAumento*12;
+            return ganhoAnual;
+        }
+        void mostrarDados(){
+            std::cout<<"Nome:"<<nome<<std::endl;
+            std::cout<<"Salario:"<<salario<<std::endl;
+            std::cout<<"Salario com aumento:"<<salarioAumento<<std::endl;
+            std::cout<<"Ganho anual:"<<ganhoAnual<<std::endl;
         }
     private:
         std::string nome;
         float salario;
-
+        float salarioAumento;
+        float ganhoAnual;
 };
 int main(){
     std::string nome;
     float  salario;
-    float valor=0;
     std::cout<<"Digite seu nome:";
     std::cin>>nome;
     std::cout<<"Digite seu salario:";
     std::cin>>salario;
     Funcionario funcionario;
-    funcionario.addAumento(valor);
+    float valor=0;
+    float salarioAumento=0;
+    funcionario.addAumento(valor,salarioAumento);
+    funcionario.mostrarDados();
     return 0;
 }
